3ed4d885f5b735a4eb18318d87f79f87c1c8e101
Mohit Singh <mohit@mohitsingh.in>
2022-10-17 08:18:49 +0530

    feat: switch to next config for react

--- index.js
+++ index.js
@@ -2,16 +2,11 @@ module.exports = {
   extends: [
     'standard',
     'standard-jsx',
-    'standard-react',
     'prettier',
     'plugin:jest/recommended',
-    'plugin:react-hooks/recommended',
+    'plugin:@next/next/recommended',
   ],
   parserOptions: {
     parser: '@babel/eslint-parser',
-  },
-  rules: {
-    'react/prop-types': 'off',
-    'react/react-in-jsx-scope': 'off',
   },
 }
